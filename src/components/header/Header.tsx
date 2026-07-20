dc7e90b8a311901f4e2a255870a9128cc7846964
ElderDragonfly <ElderDragonfly@yandex.ru>
2026-07-20 19:15:04 +0300

    feat: add header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+import logo from "../../assets/images/Rick_and_Morty-logo.png";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header__logo">
+          <img className="header__logo-image" src={logo} alt="logo" />
+          <h1 className="header__title">
+            <span className="header__title-accent--rick">Rick</span>{" "}
+            <span className="header__title-accent--and">and</span>{" "}
+            <span className="header__title-accent--morty">Morty</span>
+          </h1>
+        </div>
+        <nav className="header__navigation">
+          <a className="header__link header__link--home" href="#">
+            Home
+          </a>
+          <a className="header__link header__link--about" href="#">
+            About API
+          </a>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
