1db10c0327db0c8036681af3d09d0d5ddd2ea162
ElderDragonfly <ElderDragonfly@yandex.ru>
2026-07-29 21:59:36 +0300

    feat: connect search form to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import Header from "./components/header/Header";
 import SearchInput from "./components/search/Search";
 
 function App() {
+  // Функция-колбек для поиска
+  const handleSearch = (searchValue: string) => {
+    console.log(searchValue);
+  };
+
   return (
     <>
       <Header />
       <main>
-        <SearchInput />
+        <SearchInput onSearch={handleSearch} />
       </main>
     </>
   );
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,36 +1,57 @@
-import { Component, type ReactNode, type ChangeEvent } from "react";
+import {
+  Component,
+  type ReactNode,
+  type ChangeEvent,
+  type SubmitEvent,
+} from "react";
+
+type SearchProps = {
+  onSearch: (searchValue: string) => void;
+};
 
 type SearchState = {
   query: string;
   placeholder: string;
 };
 
-class SearchInput extends Component<object, SearchState> {
+class SearchInput extends Component<SearchProps, SearchState> {
   state: SearchState = {
     query: "",
     placeholder: "Search characters...",
   };
 
-  handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+  // Метод сохраняет текущее значение поля в state
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     this.setState({
       query: event.currentTarget.value,
     });
-    console.log(this.state.query);
+  };
+
+  // Метод вызывает переданный колбек с отправкой формы
+  handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.onSearch(this.state.query);
   };
 
   render(): ReactNode {
     return (
       <>
-        <form role="search" className="search__form">
+        <form
+          role="search"
+          className="search__form"
+          onSubmit={this.handleSubmit}
+        >
           <div className="search__input-wrapper">
             <input
               type="search"
               className="search__input"
               placeholder={this.state.placeholder}
-              onChange={this.handleSearch}
+              onChange={this.handleChange}
             />
           </div>
-          <button className="search__button">Search</button>
+          <button type="submit" className="search__button">
+            Search
+          </button>
         </form>
       </>
     );
