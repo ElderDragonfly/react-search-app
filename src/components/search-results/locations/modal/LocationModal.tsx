a2c6e510d63905bad1dc429e06a9b9599ebe9f4a
DragonflyNotepad <ElderDragonfly@yandex.ru>
2026-08-31 16:24:50 +0300

    feat: display residents in location modal

--- src/components/search-results/locations/modal/LocationModal.tsx
+++ src/components/search-results/locations/modal/LocationModal.tsx
@@ -1,13 +1,67 @@
 import { Component, type ReactNode } from "react";
-import type { Location } from "../../../types/types";
+import type { Character, Location } from "../../../types/types";
+import request from "../../../../api/apiClient";
 
 type LocationModalProps = {
   location: Location;
   onCloseModal: () => void;
+  onCharacterClick: () => void;
   //   onCharacterSelect: (character: Character) => void;
 };
 
-class LocationModal extends Component<LocationModalProps> {
+type LocationModalState = {
+  characters: Character[];
+};
+
+class LocationModal extends Component<LocationModalProps, LocationModalState> {
+  state: Readonly<LocationModalState> = {
+    characters: [],
+  };
+  // После первого рендера модального окна загружаем персонажей локации
+  componentDidMount(): void {
+    this.handleCharactersLinks(this.props.location);
+  }
+  // Обрабатываем ссылки на персонажей из локации и записываем их в state
+  handleCharactersLinks = async (location: Location) => {
+    // Извлекаем ссылки на персонажей из данных location
+    const characterUrls: string[] = location.residents;
+    // Извлекаем id персонажей
+    const characterIds: number[] = characterUrls.map((episodeUrl) => {
+      return Number(episodeUrl.split("/").at(-1));
+    });
+    try {
+      // Отправляем запрос с id персонажей
+      const characterData = await request(`/character/${characterIds}`);
+      // Записываем данные о персонажах в state locationModal
+      this.setState({
+        characters: Array.isArray(characterData)
+          ? characterData
+          : [characterData],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // Подготавливаем информацию из ссылок на персонажей для рендера
+  rederCharactersLinks = (characters: Character[]) => {
+    return (
+      <ul className="location-modal__characters">
+        {characters.map((character) => {
+          return (
+            <li key={character.id} className="location-modal__characters-item">
+              {/* При клике на эпизод вызываем коллбэк запроса App */}
+              <a
+                className="location-modal__character-name"
+                // onClick={() => this.props.onEpisodeSelect(episode)}
+              >
+                {character.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
   render(): ReactNode {
     return (
       <div className="modal" onClick={this.props.onCloseModal}>
@@ -25,10 +79,14 @@ class LocationModal extends Component<LocationModalProps> {
           </button>
           <div className="location-modal__content">
             <h2 className="location-modal__name">{this.props.location.name}</h2>
-            <p className="location-card__type">{this.props.location.type}</p>
-            <p className="location-card__dimension">
+            <p className="location-modal__type">{this.props.location.type}</p>
+            <p className="location-modal__dimension">
               {this.props.location.dimension}
             </p>
+            <div className="location-modal__residents">
+              <span className="location-modal__label">Characters:</span>
+              {this.rederCharactersLinks(this.state.characters)}
+            </div>
           </div>
         </div>
       </div>
